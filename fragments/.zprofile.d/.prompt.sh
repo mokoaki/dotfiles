971063571f02de0c964508d7cd42c61e43e76682
mokoaki <mokoriso@gmail.com>
2019-10-14 23:26:05 +0900

    wip

--- fragments/.zprofile.d/.prompt.sh
+++ fragments/.zprofile.d/.prompt.sh
@@ -4,11 +4,11 @@ source "/usr/local/opt/zsh-git-prompt/zshrc.sh"
 # シングルクォートで書くこと
 PROMPT='
 %d $(git_super_status) %n@%m %D %*
-%# '
+$ '
 
 ZSH_THEME_GIT_PROMPT_PREFIX="("
 ZSH_THEME_GIT_PROMPT_SUFFIX=")"
-ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
+ZSH_THEME_GIT_PROMPT_SEPARATOR=" "
 ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[magenta]%}"
 ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[red]%}%{● %G%}"
 ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}%{✖ %G%}"
