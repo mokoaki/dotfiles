e82b5bf23376a72368d5c0e73c4ce2198535e474
mokoaki <mokoriso@gmail.com>
2019-10-16 03:33:13 +0900

    wip

---
+++ home/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "root": true,
+  "extends": [
+    "standard"
+  ],
+  // "env": {
+  //   "es6": true
+  // },
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaVersion": 2017
+  },
+  "rules": {
+    "semi": ["off"],
+    "brace-style": [
+      "error",
+      "stroustrup"
+    ]
+  }
+}
