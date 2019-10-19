e4d7bbdddd32398e36c0ca5e6c249e6efa8cb982
mokoaki <mokoriso@gmail.com>
2019-10-19 16:00:09 +0900

    wip

--- deploy.rb
+++ deploy.rb
@@ -6,30 +6,63 @@ require "find"
 require "fileutils"
 require "pathname"
 
-current_directory = __dir__
-current_home_directory = File.join(current_directory, "home")
-home_directory = ENV["HOME"]
+# ~/からローカルのファイルへのリンクを張る
+# 冪等性があり、既存ファイルを勝手に改変したりしないように
+# 作っているつもりではある
+class Deploy
+  def initialize
+    @local_rc_directory = File.expand_path("home", __dir__)
+  end
 
-Find.find(current_home_directory) do |manage_file_path|
-  next if FileTest.directory?(manage_file_path)
-  next if File.extname(manage_file_path) == ".temp"
+  def start
+    Find.find(@local_rc_directory) do |rc_file_path|
+      next if File.ftype(rc_file_path) != "file"
+      next if File.extname(rc_file_path) == ".temp"
 
-  manage_relative_path =
-    Pathname.new(manage_file_path).relative_path_from(current_home_directory)
-
-  sym_file_path = File.join(home_directory, manage_relative_path)
-  sym_dir = File.dirname(sym_file_path)
-
-  FileUtils.mkdir_p(sym_dir)
-
-  begin
-    File.symlink(manage_file_path, sym_file_path)
-  rescue Errno::EEXIST
-    if File.readlink(sym_file_path) != manage_file_path
-      File.delete(sym_file_path)
-      retry
+      check_sym(rc_file_path)
     end
   end
 
-  puts "#{File.readlink(sym_file_path).ljust(45, ' ')} <= link #{sym_file_path}"
+  private
+
+  def check_sym(rc_file_path)
+    sym_path = sym_file_path(rc_file_path)
+
+    if FileTest.exist?(sym_path)
+      sym_exist(rc_file_path, sym_path)
+    else
+      sym_not_exist(rc_file_path, sym_path)
+    end
+  end
+
+  def sym_exist(rc_file_path, sym_path)
+    ftype = File.ftype(sym_path)
+
+    if ftype == "link" && File.readlink(sym_path) == rc_file_path
+      puts ok_message(sym_path)
+    else
+      puts "exist #{ftype} #{sym_path}"
+    end
+  end
+
+  def sym_not_exist(rc_file_path, sym_path)
+    sym_dir = File.dirname(sym_path)
+    FileUtils.mkdir_p(sym_dir) unless FileTest.exist?(sym_dir)
+    File.symlink(rc_file_path, sym_path)
+
+    puts ok_message(sym_path)
+  end
+
+  def ok_message(sym_path)
+    "#{File.readlink(sym_path).ljust(45, ' ')} <= link #{sym_path}"
+  end
+
+  def sym_file_path(rc_file_path)
+    relative_path =
+      Pathname.new(rc_file_path).relative_path_from(@local_rc_directory)
+    home_directory = ENV["HOME"]
+    File.expand_path(relative_path, home_directory)
+  end
 end
+
+Deploy.new.start
