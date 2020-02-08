9649c33c760f7866f752845c82360f432f21c1f5
mokoaki <mokoriso@gmail.com>
2020-02-08 21:31:47 +0900

    remove gems.locked gems.rb なんかrubocopも設定ファイルだけで要らないなって

--- gems.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-source "https://rubygems.org"
-
-group :development do
-  gem "rubocop", require: false
-  gem "rubocop-performance", require: false
-  # gem "solargraph", require: false
-end
